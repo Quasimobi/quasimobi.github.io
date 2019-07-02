cf030c22c48608bb7720e7db60825280544cd77f
Quasimobi <Informatie@Centobytes.nl>
2019-07-02 13:11:19 +0200

    13_11

--- _scss/blok.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Alle SCSS-en samen als 1 groot blok.
-echo = START BLOK GLINT ========================================================
-server_naar="../css" 
-
-sass b52.glint_blok.scss:$server_naar/glint_blok.css
-echo = EINDE BLOK ==============================================================
-
-
--- _scss/detail.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo = START DETAIL GLINT ======================================================
-server_naar="../css"
-sass b52.glint_detail.scss:$server_naar/glint_detail.css
-echo = EINDE DETAIL ============================================================
